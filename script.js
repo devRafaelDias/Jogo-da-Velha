5df9e28fa3e28370648bf38239309779351c6701
unknown <Rafael>
2023-05-05 14:30:56 -0300

    Verifica se deu velha

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let cabecalho = document.getElementById('cabecalho');
 let mostradorJogador = document.getElementById('jogador');
 let jogadorAtual = 'X';
 let casas = document.getElementsByTagName('td');
-
+let reinicar = document.getElementById('reiniciar')
+reinicar.style.display = 'none'
+let contVelha;
 // Grid de casas
 // 0 1 2
 // 3 4 5
@@ -36,16 +38,17 @@ function trocaJogador() {
 }
 
 function trocaTurno() {
-  let ganhou = verificaFim();
 
+  let ganhou = verificaFim();
   if (ganhou) {
     cabecalho.innerHTML = `${jogadorAtual} ganhou!`;
     bloqueiaCelulas();
-  }
+  } else
+      verificaVelha()
 }
 
 function verificaFim() {
-    let posGanha = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8]]
+    let posGanha = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8]];
     for(i of posGanha) {
         if(casas[i[0]].innerHTML == casas[i[1]].innerHTML && casas[i[1]].innerHTML == casas[i[2]].innerHTML && casas[i[1]].innerHTML !== '')
             return true;
@@ -56,8 +59,27 @@ function verificaFim() {
 function bloqueiaCelulas() {
   if(verificaFim()) {
       for(i of casas) {
-          i.textContent = ''
+        i.onclick = false
       }
+      reinicar.style.display = 'block'
   }
-  return false;
-} 
+}
+function verificaVelha() {
+  contVelha = 0;
+  for(i of casas) {
+    if(i.textContent != ''){
+      contVelha++
+    }
+  }
+  if(contVelha == 9) {
+    alert("Deu velha")
+    reinicar.style.display = 'block'
+  }
+}
+function refresh() {
+  for(i of casas) {
+    i.textContent = ''
+    i.onclick = jogar
+}
+  reinicar.style.display = 'none'
+}
